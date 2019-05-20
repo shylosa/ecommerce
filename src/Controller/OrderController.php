eb1180fd6cfe4dc086be601afbce19c1c4786fa8
Serhii Shylo <shilkas2@gmail.com>
2019-05-20 23:00:49 +0300

    Обновление корзины и перенос JS в ассеты

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\OrderItem;
 use App\Entity\Product;
 use App\Service\OrdersService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,32 +16,44 @@ class OrderController extends AbstractController
      */
     public function addToCart(Product $product, OrdersService $ordersService, Request $request)
     {
-		$ordersService->addToCart($product);
-
-		if($request->isXmlHttpRequest()){
-		    return $this->headerCart($ordersService);
+        $ordersService->addToCart($product);
+        if ($request->isXmlHttpRequest()) {
+            return $this->headerCart($ordersService);
         }
-
-		$referer = $request->headers->get('Referer');
-
+        $referer = $request->headers->get('Referer');
         return $this->redirect($referer);
     }
-
     /**
      * @Route("/cart", name="order_cart")
      */
     public function cart(OrdersService $ordersService)
     {
         return $this->render('order/cart.html.twig', [
-            'order' => $ordersService->getOrderFromCart()
+            'order' => $ordersService->getOrderFromCart(),
         ]);
     }
-
     public function headerCart(OrdersService $ordersService)
     {
         return $this->render('order/headerCart.html.twig', [
             'order' => $ordersService->getOrderFromCart(),
         ]);
     }
+    /**
+     * @Route("/cart/update-count/{id}", name="order_update_count")
+     */
+    public function updateCount(OrderItem $orderItem, OrdersService $ordersService, Request $request)
+    {
+        $order = $ordersService->getOrderFromCart();
 
+        if ($orderItem->getOrder() !== $order) {
+            return $this->createAccessDeniedException('Invalid order item');
+        }
+
+        $count = $request->request->getInt('count');
+        $ordersService->setCount($orderItem, $count);
+
+        return $this->render('order/cartTable.html.twig', [
+            'order' => $order,
+        ]);
+    }
 }
--- src/Service/OrdersService.php
+++ src/Service/OrdersService.php
@@ -11,63 +11,55 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class OrdersService
 {
-	const CART_SESSION_KEY = 'cart';
-
-	private $session;
-
-	private $orderRepository;
-
-	private $entityManager;
-
-	public function __construct(
-		SessionInterface $session,
-		OrderRepository $orderRepository,
-		EntityManagerInterface $entityManager
-	) {
-		$this->session = $session;
-		$this->orderRepository = $orderRepository;
-		$this->entityManager = $entityManager;
-	}
-
-	public function getOrderFromCart()
-	{
-		$order = null;
-		$orderId = $this->session->get(self::CART_SESSION_KEY);
-
-		if ($orderId) {
-			$order = $this->orderRepository->find($orderId);
-		}
-
-		if (!$order) {
-			$order = new Order();
-		}
-
-		return $order;
-	}
-
-	public function addToCart(Product $product, int $count = 1): Order
-	{
-		$order = $this->getOrderFromCart();
-		$orderItem = null;
-
-		foreach ($order->getOrderItems() as $item) {
-			if ($item->getProduct() === $product) {
-				$orderItem = $item;
-			}
-		}
-
-		if (!$orderItem) {
-			$orderItem = new OrderItem();
-			$orderItem->setProduct($product);
-			$order->addOrderItem($orderItem);
-		}
-
-		$orderItem->setCount($orderItem->getCount() + $count);
-		$this->entityManager->persist($order);
-		$this->entityManager->flush();
-		$this->session->set(self::CART_SESSION_KEY, $order->getId());
-
-		return $order;
-	}
-
+    const CART_SESSION_KEY = 'cart';
+    private $session;
+    private $orderRepository;
+    private $entityManager;
+    public function __construct(
+        SessionInterface $session,
+        OrderRepository $orderRepository,
+        EntityManagerInterface $entityManager
+    ) {
+        $this->session = $session;
+        $this->orderRepository = $orderRepository;
+        $this->entityManager = $entityManager;
+    }
+    public function getOrderFromCart()
+    {
+        $order = null;
+        $orderId = $this->session->get(self::CART_SESSION_KEY);
+        if ($orderId) {
+            $order = $this->orderRepository->find($orderId);
+        }
+        if (!$order) {
+            $order = new Order();
+        }
+        return $order;
+    }
+    public function addToCart(Product $product, int $count = 1): Order
+    {
+        $order = $this->getOrderFromCart();
+        $orderItem = null;
+        foreach ($order->getOrderItems() as $item) {
+            if ($item->getProduct() === $product) {
+                $orderItem = $item;
+            }
+        }
+        if (!$orderItem) {
+            $orderItem = new OrderItem();
+            $orderItem->setProduct($product);
+            $order->addOrderItem($orderItem);
+        }
+        $orderItem->setCount($orderItem->getCount() + $count);
+        $this->entityManager->persist($order);
+        $this->entityManager->flush();
+        $this->session->set(self::CART_SESSION_KEY, $order->getId());
+        return $order;
+    }
+    public function setCount(OrderItem $orderItem, int $count): Order
+    {
+        $orderItem->setCount($count);
+        $this->entityManager->flush();
+        return $orderItem->getOrder();
+    }
 }
