d1b5d5fb16137e2d7ac564e4328b8a6b45b4720f
Serhii Shylo <shilkas2@gmail.com>
2019-05-21 09:04:57 +0300

    Исправлена работоспособность webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
-var Encore = require('@symfony/webpack-encore');
+
+const Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+//var Encore = require('@symfony/webpack-encore');
 
 Encore
 // directory where compiled assets will be stored
